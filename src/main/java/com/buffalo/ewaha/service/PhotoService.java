bb08f0c42a76df060294a863f618d67846b2ba0a
Jakub Glinka <glinka.jakub@outlook.com>
2021-01-20 16:22:20 +0100

    add transactions

--- src/main/java/com/buffalo/ewaha/service/PhotoService.java
+++ src/main/java/com/buffalo/ewaha/service/PhotoService.java
@@ -7,12 +7,14 @@ import com.azure.storage.blob.specialized.BlockBlobClient;
 import com.buffalo.ewaha.controller.dto.PhotoDto;
 import com.buffalo.ewaha.model.Station;
 import com.buffalo.ewaha.repository.IStationRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.ByteArrayInputStream;
@@ -20,13 +22,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
+@Slf4j
 @Service
+@Transactional
 public class PhotoService implements IPhotoService{
 
     @Autowired
     private IStationRepository stationRepository;
 
     @Override
+    @Transactional
     public void savePhoto(PhotoDto photoDto) throws IOException {
         //TODO send to blobstorage
         Station station = stationRepository.findByLocalization(photoDto.getLocalization());
@@ -36,34 +41,30 @@ public class PhotoService implements IPhotoService{
                     .localization(photoDto.getLocalization())
                     .build();
             stationRepository.save(station);
+            log.info("saved location in db");
         }
             //TODO send to blobstorage
-        System.out.println("Start blob");
         String yourSasToken = "?sv=2019-12-12&ss=b&srt=sco&sp=rwdlacx&se=2021-01-22T23:57:15Z&st=2021-01-18T15:57:15Z&spr=https,http&sig=XSV0gOkohJn6na0x%2FdNACmVwlHX80OWabyL2zZ9%2F5s0%3D";
         /* Create a new BlobServiceClient with a SAS Token */
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
                 .endpoint("https://ewahastorage.blob.core.windows.net/blob")
                 .sasToken(yourSasToken)
                 .buildClient();
-        System.out.println("Connected to service: " + blobServiceClient.getAccountInfo());
+        log.info("Connected to blobService " + blobServiceClient.getAccountInfo());
         /* Create a new container client */
         BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient("blob");
-        System.out.println("Connected to container: " + containerClient.getBlobContainerName());
+        log.info("Connected to container: " + containerClient.getBlobContainerName());
 
         /* Upload the file to the container */
         System.out.println(containerClient.getBlobContainerUrl());
         BlockBlobClient blockBlobClient = containerClient.getBlobClient(UUID.randomUUID().toString()).getBlockBlobClient();
-        String dataSample = "samples";
-        System.out.println("account info: " + blobServiceClient.getAccountInfo());
         String url = "";
         try (ByteArrayInputStream dataStream = new ByteArrayInputStream(photoDto.getPhoto().getBytes())) {
 
             blockBlobClient.upload(dataStream, photoDto.getPhoto().getSize());
             url = blockBlobClient.getBlobUrl();
+            log.info("uploaded file to blob storage and return url: " + url);
         }
-//        blobClient.uploadFromFile("../../resources/images.png");
-//        blobClient.upload(photoDto.getPhoto().getInputStream(), photoDto.getPhoto().getSize());
-        System.out.println("uploaded file: " + url);
         sendUrlToApi2(photoDto.getLocalization(), url);
 //        System.out.println("req: " + sendUrlToApi2(photoDto.getLocalization(), url));
     }
@@ -79,7 +80,7 @@ public class PhotoService implements IPhotoService{
                 entity,
                 String.class
         );
-        System.out.println("Test");
+        log.info("sended request to verysmartapi");
         return responseEntity.getStatusCode().toString();
     }
 }
--- src/main/java/com/buffalo/ewaha/service/StationService.java
+++ src/main/java/com/buffalo/ewaha/service/StationService.java
@@ -5,21 +5,27 @@ import com.buffalo.ewaha.model.Station;
 import com.buffalo.ewaha.repository.IStationRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
+@Transactional
 public class StationService implements IStationService{
 
     @Autowired
     IStationRepository stationRepository;
 
     @Override
+    @Transactional
     public List<StationDto> findAll() {
         List<Station> list = stationRepository.findAll();
+        log.info("get all prices");
         return list.stream().map(this::mapToDto).collect(Collectors.toList());
     }
 
